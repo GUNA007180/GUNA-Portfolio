dfae5f99f1ccc49f70480dd4e9b28c5975c595a8
GUNA007180 <guna51815@gmail.com>
2024-12-22 12:13:13 +0530

    Update Contact

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -5,7 +5,7 @@ import gmail from './picture/email.png'
 function Contact() {
     return (
        <section id='contact'>
-         <div class='bg-sec h-3/6' style={{background:"radial-gradient(circle,rgba(73,22,94,1)25%,rgba(0,0,0,1)100%)",backgroundSize: "cover"}} >
+         <div class='h-3/6' style={{background:"radial-gradient(circle,rgba(73,22,94,1)25%,rgba(0,0,0,1)100%)",backgroundSize: "cover"}} >
             <h1 className='text-2xl  text-green-300 pl-16 font-bold' >Contact</h1>
             <div className='place-items-center'>
 
